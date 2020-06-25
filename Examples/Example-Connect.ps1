cf39afb645dd7c60747607f4d0841eeb2d2d26e7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-06-25 23:41:24 +0200

    Update

--- Examples/Example-Connect.ps1
+++ Examples/Example-Connect.ps1
@@ -1,3 +1,17 @@
-﻿Import-Module .\Connectimo.psd1 -Force
+﻿#Import-Module .\Connectimo.psd1 -Force
 
-Connect-WinConnectivity -MultiFactorAuthentication -Service All
+#Connect-WinConnectivity -MultiFactorAuthentication -Service AzureAD,MSOnline,ExchangeOnline
+
+#$Permission = Get-MailboxPermission -Identity 'spes@spes.org.pl'
+$Permission | Format-Table -AutoSize *
+$Permission | Where-Object { $_.AccessRights -eq "FullAccess" -and $_.IsInherited -eq $false }
+
+
+
+#$FixAutoMapping = Get-MailboxPermission -Identity sharedmailbox | Where-Object { $_.AccessRights -eq "FullAccess" -and $_.IsInherited -eq $false }
+#$FixAutoMapping | Remove-MailboxPermission
+#$FixAutoMapping | ForEach-Object {
+#    Add-MailboxPermission -Identity $_.Identity -User $_.User -AccessRights:FullAccess -AutoMapping $false
+#}
+
+Get-Help -
